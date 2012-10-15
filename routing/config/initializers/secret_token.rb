0ecd42b41c1f4cd9e1c34070ae377e5bdf59d711
Allen Leis <allen.leis@threespot.com>
2012-10-15 11:52:28 -0400

    initial commit; working version of routing dynamic pages

---
+++ routing/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Routing::Application.config.secret_token = 'b5b298267f3fa415e51cd6ca5a7d4ef5206302cda362e254d5cfa76b3463ff888f43b0802bd1133db2d916922c5a44db22ca345b0e1d036490c030dd13f03d12'
